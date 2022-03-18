0b52200e31ab90f43019e471f67626f220322ffb
GlebShekovtsov <80450328+GlebShekovtsov@users.noreply.github.com>
2022-03-18 11:51:23 +0300

    Add files via upload

--- pizza.sql
+++ pizza.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Мар 10 2022 г., 20:14
+-- Время создания: Мар 17 2022 г., 23:41
 -- Версия сервера: 8.0.19
 -- Версия PHP: 7.4.14
 
@@ -92,7 +92,28 @@ INSERT INTO `site` (`id`, `title`, `content`) VALUES
 (1, 'О нас', '<div class=\"content\">\r\n            <h1>Мы</h1>\r\n            <p>\r\n                Обычно люди приходят в Pizza House, чтобы просто поесть. Наши промоутеры раздают листовки про кусочек пиццы за двадцать рублей или ещё что-то выгодное. Мы делаем это как первый шаг, чтобы познакомиться.\r\n            </p>\r\n            <p>\r\n                Но для нас Pizza House — не только пицца. Это и пицца тоже, но в первую очередь это большое дело, которое вдохновляет нас, заставляет каждое утро просыпаться и с интересом продолжать работу.\r\n            </p>\r\n            <h1>Идеальные ингредиенты</h1>\r\n            <div class=\"cont\">\r\n\r\n                <div id=\"block\">\r\n                    <h2>Тесто</h2>\r\n                    <p>Самая тонкая вещь. Главное -</p>\r\n                    <p>сделать его «живым».</p>\r\n                    <p>Это целый квест из граммов, градусов,</p>\r\n                    <p>процентов и часов с минутами.</p>\r\n                    <p>Процесс непростой, но у нас получается!</p>\r\n                </div>\r\n                <div id=\"block\">\r\n                <h2>Моцарелла</h2>\r\n                    <p>Сыр в пицце - ключевой</p>\r\n                    <p>ингредиент. Мы используем</p>\r\n                    <p>настоящую моцареллу от</p>\r\n                    <p>российских поставщиков. Сыр</p>\r\n                    <p>должен тянуться, как на</p>\r\n                    <p>картинке.</p>\r\n                </div>\r\n                <div id=\"block\">\r\n                    <h2>Начинка</h2>\r\n                    <p>Есть два главных правила вкусной начинки:</p>\r\n                    <p>не экономить на начинке;</p>\r\n                    <p>фанатично соблюдать режим хранения.</p>\r\n                    <p>Это и весь секрет.</p>\r\n                </div>\r\n                <div id=\"block\">\r\n                    <h2>Томатный соус</h2>\r\n                    <p>Главное, что нужно знать про</p>\r\n                    <p>хороший томатный соус: он</p>\r\n                    <p>должен быть сделан из томатов.</p>\r\n                    <p>Звучит логично, но</p>\r\n                    <p>задумайтесь!</p>\r\n                </div>\r\n            </div>\r\n        </div>'),
 (2, 'Контакты', '<div class=\"cont\">\r\n\r\n            <div id=\"block\">\r\n            <h2>Кировский</h2>\r\n            <p>ул. Кирова, 149 В</p>\r\n            <p>Доставка и самовывоз</p>\r\n            <p>09:00 — 23:30</p>\r\n            <p>Ресторан</p>\r\n            <p>09:00 — 23:00</p>\r\n            </div>\r\n            <div id=\"block\">\r\n            <h2>Центр Аллея Героев</h2>\r\n            <p>ул. Аллея Героев, 5</p>\r\n            <p>Доставка и самовывоз</p>\r\n            <p>Пн - Чт: 09:00 — 23:30</p>\r\n            <p>Пт - Вс: 09:00 — 00:00</p>\r\n            <p>Ресторан</p>\r\n            <p>Пн - Чт: 09:00 — 23:00</p>\r\n            <p>Пт - Вс: 09:00 — 00:00</p>\r\n            </div>\r\n            <div id=\"block\">\r\n            <h2>Красноармейский</h2>\r\n            <p>пр-т им Героев Сталинграда, 56Б</p>\r\n            <p>Доставка и самовывоз</p>\r\n            <p>09:00 — 22:00</p>\r\n            </div>\r\n            <div id=\"block\">\r\n            <h2>Красный Октябрь</h2>\r\n            <p>пр-т имени В.И. Ленина, 111г</p>\r\n            <p>Доставка и самовывоз</p>\r\n            <p>09:00 — 00:00</p>\r\n            <p>Ресторан</p>\r\n            <p>09:00 — 00:00</p>\r\n            </div>\r\n        </div>'),
 (4, 'Меню', ''),
-(5, 'Акции', '');
+(5, 'Акции', ''),
+(11, 'Регистрация', '');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `password`) VALUES
+(1, 'jmix', '$2y$10$v.vDhnR6d6Z.H9wk.xmxFOpLIQ3P4.M5g5QI3wRXmMht3ws1eW2.q'),
+(4, '123', '$2y$10$VYOu5yjBbI.wKlvOVLJ04.uwV/roiNMBAVLyr.0sCEXRjhRyFOILS');
 
 --
 -- Индексы сохранённых таблиц
@@ -117,6 +138,12 @@ ALTER TABLE `site`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -136,7 +163,13 @@ ALTER TABLE `promotion`
 -- AUTO_INCREMENT для таблицы `site`
 --
 ALTER TABLE `site`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
